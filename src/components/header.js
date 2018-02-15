d4ec7589829b25f48310623b366172e176f14c6d
Vipin Lahoti <vipinl@ispace.com>
2018-02-15 14:18:59 +0530

    logo edited

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const Header = () => (
   <nav className="navbar is-transparent is-fixed-top">
     <div className="navbar-brand">
-      <Link className="navbar-item" to="/">PintoBin</Link>
+      <Link className="navbar-item" to="/">P<span>.</span></Link>
     </div>
   </nav>
 )
