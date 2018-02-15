c4e4b66028a111c5fe2348bfe74850e8ecf15523
vipinlahoti <vipi.nsl2787@gmail.com>
2018-02-15 19:56:07 +0530

    middle align 404 fixed

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,9 +3,13 @@ import React from 'react'
 const NotFoundPage = () => (
   <section className="hero is-fullheight">
     <div className="hero-body">
-      <div className="container">
-        <h1 className="title">404</h1>
-        <h2 className="subtitle">You just hit a route that doesn&#39;t exist... the sadness.</h2>
+      <div className="columns">
+        <div className="column">
+          <div className="hero-desc">
+            <h1 className="title">404</h1>
+            <h2 className="subtitle">You just hit a route that doesn&#39;t exist... the sadness.</h2>
+          </div>
+        </div>
       </div>
     </div>
   </section>
