4a5d0192392712bc72dc3f63f7fb504f908fa80b
vipinlahoti <vipi.nsl2787@gmail.com>
2018-02-15 19:31:42 +0530

    className fixed

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,10 @@ import React from 'react'
 
 const NotFoundPage = () => (
   <section className="hero is-fullheight">
-    <div class="hero-body">
-      <div class="container">
-        <h1 class="title">
-          404
-        </h1>
-        <h2 class="subtitle">You just hit a route that doesn&#39;t exist... the sadness.</h2>
+    <div className="hero-body">
+      <div className="container">
+        <h1 className="title">404</h1>
+        <h2 className="subtitle">You just hit a route that doesn&#39;t exist... the sadness.</h2>
       </div>
     </div>
   </section>
